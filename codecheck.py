4032cafdf65483c673660af566a1377bc223fc8d
sheebanair1981 <sheebanair1981@gmail.com>
2023-10-21 18:52:06 -0500

    checking codes

---
+++ codecheck.py
@@ -0,0 +1,9 @@
+#Write a Python function to find the factorial of a non-negative integer.
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(7))
+
